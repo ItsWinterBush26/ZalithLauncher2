3461809426c255c9b6d75d8dcf933671f18e037d
ItsWinterBush <154499206+ItsWinterBush26@users.noreply.github.com>
2025-08-08 19:55:56 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,3 +17,53 @@ buildscript {
         classpath(libs.stringfog.xor)
     }
 }
+dependencies {
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("commons-codec:commons-codec:1.17.1")
+    // implementation("com.wu-man:android-bsf-api:3.1.3")
+    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0-beta01")
+    implementation("androidx.annotation:annotation:1.7.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.palette:palette-ktx:1.0.0")
+
+    implementation("com.github.duanhong169:checkerboarddrawable:1.0.2")
+    implementation("com.github.PojavLauncherTeam:portrait-sdp:ed33e89cbc")
+    implementation("com.github.PojavLauncherTeam:portrait-ssp:6c02fd739b")
+    implementation("com.github.Mathias-Boulay:ExtendedView:1.0.0")
+    implementation("com.github.Mathias-Boulay:android_gamepad_remapper:2.0.3")
+    implementation("com.github.Mathias-Boulay:virtual-joystick-android:1.14")
+    implementation("com.github.skydoves:powerspinner:1.2.7")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.github.angcyo.DslTablayout:TabLayout:3.6.5")
+
+    implementation("com.github.megatronking.stringfog:xor:5.0.0")
+
+    implementation("top.fifthlight.touchcontroller:proxy-client-android:0.0.2")
+
+    // implementation("com.intuit.sdp:sdp-android:1.0.5")
+    // implementation("com.intuit.ssp:ssp-android:1.0.5")
+
+    implementation("org.tukaani:xz:1.9")
+    // Our version of exp4j can be built from source at
+    // https://github.com/PojavLauncherTeam/exp4j
+    implementation("net.sourceforge.htmlcleaner:htmlcleaner:2.6.1")
+    implementation("com.bytedance:bytehook:1.0.10")
+
+    // implementation("net.sourceforge.streamsupport:streamsupport-cfuture:1.7.0")
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("org.commonmark:commonmark:0.19.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.flexbox:flexbox:3.0.0")
+
+    implementation("com.getkeepsafe.taptargetview:taptargetview:1.14.0")
+    implementation("io.github.petterpx:floatingx:2.3.3")
+    implementation("org.greenrobot:eventbus:3.3.1")
+    implementation("com.moandjiezana.toml:toml4j:0.7.2") {
+        exclude(group = "com.google.code.gson", module = "gson")
+    }
+}
